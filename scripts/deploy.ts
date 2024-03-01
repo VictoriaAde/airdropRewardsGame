a048612ecf9bbbc08066b6be1a8019ff61b0b7c6
Victoria <adedayovicky123@gmail.com>
2024-03-01 16:08:22 +0100

    SCRIPTS: Update deploy script

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,13 +1,12 @@
 import { ethers } from "hardhat";
 
 async function main() {
-  const subID = 9762;
-  const vrfCor = "0x8103b0a8a00be2ddc778e6e7eaa21791cd364625";
   const tokenAddr = "0x07d1ad5B9F0752527e37b065a4752aEdc28D0457";
+  const subID = 9762;
 
   const airdropRewards = await ethers.deployContract(
     "AirdropRewardGame",
-    [vrfCor, tokenAddr, subID],
+    [tokenAddr, subID],
     {}
   );
 
